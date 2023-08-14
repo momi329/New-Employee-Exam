473708ca44dc19ab96a66f780f43f99705d86b64
momi329 <amekaka@gmail.com>
2023-08-14 17:54:50 +0800

    layout+onclick

--- test3/app.js
+++ test3/app.js
@@ -1,28 +1,42 @@
-$(document)
-  .ready(function () {
-    console.log("success");
-  })
-  .$(".frzTable.default")
-  .frzTable({
-    count: {
-      // M版時每次點擊往前往後移動幾格儲存格
-      slide: 1, // [number]
-      // M版時一個畫面show幾格儲存格
-      show: 4, // [number]
-    },
-    // 設定花多久時間移動完成
-    speed: 0.3, // [number]
-    // 每次點擊儲存格時會執行此callback，並帶入所點擊的儲存格jquery物件
-    whenClick: function ($element) {
-      console.log($element);
-    },
+$(function () {
+  $(".price").on("click", function () {
+    const clickedIndex = $(this).index();
+    const rowIndex = $(this).parent().index() - 1;
+    if ($(this).hasClass("clicked")) {
+      $(this).toggleClass("clicked");
+      $.each(
+        $(this).parents("section").find(`.form-row`),
+        function (index, item) {
+          if (rowIndex !== index) {
+            $(item).find(`div:eq(${clickedIndex})`).toggleClass("background");
+          }
+        }
+      );
+    } else {
+      $.each($("section"), function (index, item) {
+        $.each(
+          $(item).children("div").find(".clicked"),
+          function (divIndex, itemDiv) {
+            $(itemDiv).removeClass("clicked");
+          }
+        );
+        $.each(
+          $(item).children("div").find(".background"),
+          function (divIndex, itemDiv) {
+            $(itemDiv).removeClass("background");
+          }
+        );
+      });
+      $(this).toggleClass("clicked");
+      $.each(
+        $(this).parents("section").find(`.form-row`),
+        function (index, item) {
+          if (rowIndex !== index) {
+            $(item).find(`div:eq(${clickedIndex})`).toggleClass("background");
+          }
+        }
+      );
+    }
+    $(this).siblings(".price").toggleClass("background");
   });
-$(".frzTable.rel").frzTable({
-  count: {
-    slide: 1,
-    show: 2,
-  },
-  whenClick: function ($element) {
-    // console.log($element)
-  },
 });
